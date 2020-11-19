94ed0f3400ccb6856661690b5a2838f2015f0384
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2020-11-19 15:49:14 +0100

    js Fizz Buzz Finito

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
 
  
 for (var i = 1; i <= 100; i++) {
-    var f = i % 3 == 0, b = i % 5 == 0;
-    console.log(f ? b ? "FizzBuzz" : "Fizz" : b ? "Buzz" : i);
-  } 
+    if (i%3 == 0 && i%5 == 0) {
+        console.log("FizzBuzz");
+    }else if (i %5 == 0){
+        console.log("Buzz");
+    }else if (i %3 ==0){
+        console.log("Fizz");
+    }else{
+        console.log(i);
+    }
+}
+   
+   
